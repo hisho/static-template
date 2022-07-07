1ae5334f1c180d371994586c6629aff8c64a28a7
hisho <hisho.web@gmail.com>
2022-07-07 17:57:05 +0900

    ✨ 不要な記述を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Head } from "minista"
-import { Navigate } from "@src/component/functional/Navigate/Navigate"
 
 export const Index = () => {
-  console.log(process.env["NODE_ENV"])
   return (
     <>
       <Head title={"Home"} />
