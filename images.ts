a3ee4c11e4183609cff0efb1843aa2833b321f07
hisho <hisho.web@gmail.com>
2022-07-10 14:10:42 +0900

    ✨ fileCopyのtaskを作成

--- images.ts
+++ images.ts
@@ -2,13 +2,47 @@ import minimist from "minimist"
 import chokidar from "chokidar"
 import path from "path"
 import glob from "glob"
-
-const write = async (path: string) => {
-  //TODO:実際の処理を書く
-  console.log("write", path)
-}
+import fs from "fs"
+import util from "util"
 
 const src = "src/images/"
+const dest = "public/images/"
+
+const copyFilePromise = util.promisify(fs.copyFile)
+const copyFiles: (
+  srcDir: string,
+  destDir: string,
+  fileNames: string[]
+) => Promise<Awaited<unknown>[]> = (srcDir, destDir, fileNames) => {
+  return Promise.all(
+    fileNames.map((fineName) => {
+      return copyFilePromise(srcDir, path.join(destDir, fineName))
+    })
+  )
+}
+
+const write = async (currentPath: string) => {
+  const fileName = path.basename(currentPath)
+
+  /**
+   * srcのpathを割り出してdistのpathを連結する
+   * 'src/images/hoge.png' -> 'public/images/hoge.png'
+   * 'src/images/hoge/fuga.png' -> 'public/images/hoge/fuga.png'
+   */
+  const destPath = path
+    .join(dest, path.dirname(currentPath), "/")
+    .replace(new RegExp(src), "")
+
+  if (!fs.existsSync(destPath)) {
+    fs.mkdirSync(destPath, { recursive: true })
+  }
+
+  try {
+    await copyFiles(currentPath, destPath, [fileName, `${fileName}.webp`])
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 const main = async () => {
   // @ts-ignore
@@ -21,7 +55,7 @@ const main = async () => {
   if (argv.watch !== undefined) {
     chokidar
       .watch(
-        ["**/*.png", "**/*.jpg"].map((n) => `${src}${n}`),
+        ["**/*.png", "**/*.jpg"].map((n) => path.join(src, n)),
         {
           ignoreInitial: true,
         }
@@ -40,7 +74,7 @@ const main = async () => {
       .sync(`**/*.+(png|jpg)`, {
         cwd: path.resolve(process.cwd(), src),
       })
-      .map((n) => src + n)
+      .map((n) => path.join(src, n))
 
     paths.forEach((path) => {
       console.log("Building...", path)
